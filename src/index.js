a96cae118669096961e16b97b5cd9db4942d6bf1
CJ Sampson <cjsampson41@gmail.com>
2016-12-21 14:24:53 -0700

    add key prop to individual weather list items

--- src/index.js
+++ src/index.js
@@ -4,30 +4,37 @@ import axios from 'axios';
 
 
 class App extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            data: null
+            data: []
         };
     }
 
     componentWillMount () {
         axios.get("http://api.openweathermap.org/data/2.5/forecast?q=SaltLake&APPID=e6d03308e46ef159cfbf5f857a6798a5")
         .then(response => {
-            console.log(response)
-            this.setState({ data: response.data })
+            this.setState({ data: response.data.list })
+            console.log(this.state.data)
         })
-        .catch(response => console.log(response))
+        .catch(err => console.log(err, response))
     }
 
+
     render() {
+        const data = this.state.data
+        
         return (
             <div>
-            
+                {data.map((item) => 
+                    <li key={item.dt}>
+                        {item.dt_txt}
+                    </li>)
+                } 
             </div>
         );
     }
-    
+        
 }
 
 
