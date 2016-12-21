14eb569a91276284a75cc9721892643c40db6551
CJ Sampson <cjsampson41@gmail.com>
2016-12-21 11:49:16 -0700

    clean up axios response call

--- src/index.js
+++ src/index.js
@@ -2,16 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
-const apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=SaltLake&APPID=e6d03308e46ef159cfbf5f857a6798a5`
-axios({
-  method: 'get',
-  url: apiURL,
-})
-.then(function (response) {
-    console.log(response);
-})
-  .catch(function (error) {
-    console.log(error);
-  });
 
-  
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            data: null
+        };
+    }
+
+    componentWillMount () {
+        axios.get("http://api.openweathermap.org/data/2.5/forecast?q=SaltLake&APPID=e6d03308e46ef159cfbf5f857a6798a5")
+        .then(response => {
+            console.log(response)
+            this.setState({ data: response.data })
+        })
+        .catch(response => console.log(response))
+    }
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        );
+    }
+    
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
