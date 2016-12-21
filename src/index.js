66bf52f712400dc4cccd5137f4fb4e9511003dde
CJ Sampson <cjsampson41@gmail.com>
2016-12-20 22:41:28 -0700

    axios response for 5day 3hour response

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-// Set up your application entry point here...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+const apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=SaltLake&APPID=e6d03308e46ef159cfbf5f857a6798a5`
+axios({
+  method: 'get',
+  url: apiURL,
+})
+.then(function (response) {
+    console.log(response);
+})
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  
