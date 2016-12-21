296c648678f5977cbef5fac64e2e5f4e2b0a28a3
CJ Sampson <cjsampson41@gmail.com>
2016-12-21 15:14:13 -0700

    styles for the weather data boxes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+import './styles/weather.sass';
+
+// How to get icon URL
+// For code 501 - moderate rain icon = "10d" 
+// URL is
+// http://openweathermap.org/img/w/10d.png
 
 class App extends React.Component {
     constructor(props) {
@@ -14,21 +20,23 @@ class App extends React.Component {
     componentWillMount () {
         axios.get("http://api.openweathermap.org/data/2.5/forecast?q=SaltLake&APPID=e6d03308e46ef159cfbf5f857a6798a5")
         .then(response => {
-            this.setState({ data: response.data.list })
-            console.log(this.state.data)
+            this.setState({ data: response.data.list });
+            console.log(this.state.data);
         })
-        .catch(err => console.log(err, response))
+        .catch(err => console.log(err));
     }
 
 
     render() {
-        const data = this.state.data
+        const data = this.state.data;
         
         return (
-            <div>
+            <div className="root">
                 {data.map((item) => 
-                    <li key={item.dt}>
-                        {item.dt_txt}
+                    <li className="weather--list--item" key={item.dt}>
+                        <span>{item.dt_txt}</span>
+                        <img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} />
+
                     </li>)
                 } 
             </div>
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -47,7 +47,7 @@ export default {
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
       {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
       {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
-      {test: /(\.css|\.scss)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']},
+      {test: /(\.css|\.scss|\.sass)$/, loaders: ['style', 'css?sourceMap', 'postcss', 'sass?sourceMap']},
       {test: /\.json$/, loader: "json"}
     ]
   },
