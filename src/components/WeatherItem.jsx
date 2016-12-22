cba27eb98ad2b09e440e8fb0c2e47de7d41280ae
CJ Sampson <cjsampson41@gmail.com>
2016-12-21 18:46:35 -0700

    cover image for full background

--- src/components/WeatherItem.jsx
+++ src/components/WeatherItem.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const WeatherItem = (data) => {
     const weatherData = data.data;
-    
+
     return (
-        <div>
+        <div className="weather--list-container">
             {weatherData.map((item) => 
                 <li className="weather--list--item" key={item.dt}>
                     <span>{item.dt_txt}</span>
-                    <img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} />
-                    <span>{Math.ceil(item.main.temp)}</span>
+                    <figure>
+                        <img src={`http://openweathermap.org/img/w/${item.weather[0].icon}.png`} />
+                    </figure>
+                    <p>Temperature: <span>{Math.ceil(item.main.temp)}</span></p>
                 </li>
             )}                        
         </div>
